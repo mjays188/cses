236e97109fba527aa43a393bf1a8151540116e3e
mrsankhl <mrityunjay.s.sankhla@oracle.com>
2024-12-08 18:18:57 +0530

    add cpp template

---
+++ cpp_template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+int main(){
+	ll n;
+	cin>>n;
+	vector<ll> v;
+	for(ll i=0;i<n;i++){
+		ll t;
+		cin>>t;
+		v.push_back(t);
+	}
+	return 0;
+}
