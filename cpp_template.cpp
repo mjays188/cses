db8e1bd4cf7a8286fb571862b1c8922e49f77a6b
mrsankhl <mrityunjay.s.sankhla@oracle.com>
2024-12-08 18:21:52 +0530

    update c++ template

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 int main(){
-	cout<<"\nHello world";
+	ll n;
+	cin>>n;
+	vector<ll> v;
+	for(ll i=0;i<n;i++){
+		ll t;
+		cin>>t;
+		v.push_back(t);
+	}
 	return 0;
 }
