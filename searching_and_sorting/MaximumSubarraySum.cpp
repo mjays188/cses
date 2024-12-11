ff0757dff34c50c2942f7701f26711282330ed97
mrsankhl <mrityunjay.s.sankhla@oracle.com>
2024-12-11 10:33:24 +0530

    add explaination comments - Maximum Subarray Sum

--- searching_and_sorting/MaximumSubarraySum.cpp
+++ searching_and_sorting/MaximumSubarraySum.cpp
@@ -14,16 +14,19 @@ int main(){
 		cin>>t;
 		v.push_back(t);
 	}
-	
+	// declaring array mx: which stores the maximumSubarraySum till ith index from beginning
 	vector<ll> mx(n, INT_MIN);
+	// if there's only 1 element in incoming array, then maximumSubarraySum as the on & only element
 	mx[0] = v[0];
 	for(ll i=1;i<n;i++){
+		// if maximumSubarraySum till now is negative, then start considering array starting from ith idx for maximumSubarraySum  
 		if(mx[i-1] < 0)
 			mx[i] = v[i];
+		// add ith element to max sum subarray ending at (i-1)th idx
 		else
 			mx[i] = v[i]+mx[i-1];
-		//mx[i] = max(0ll, v[i]+mx[i-1]);
 	}
+	// to get  maximumSubarraySum fetch the maximum Sum from mx array
 	ll ans = *max_element(mx.begin(), mx.end());
 	cout<<ans;
 	return 0;
