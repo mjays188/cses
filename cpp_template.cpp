91a087131a0424f3968c8ed2b79bb3f6e27f220b
mrsankhl <mrityunjay.s.sankhla@oracle.com>
2025-01-03 18:27:33 +0530

    sync cpp_template with main

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
 int main(){
-	cout<<"\nHello world";
+	// /opt/homebrew/Cellar/gcc/14.2.0_1/bin - directory for gcc compiler in mac
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+  	cout.tie(nullptr);
+	ll n;
+	cin>>n;
+	vector<ll> v;
+	for(ll i=0;i<n;i++){
+		ll t;
+		cin>>t;
+		v.push_back(t);
+	}
 	return 0;
 }
